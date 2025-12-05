f3b8c2e89ae52a856b44ef904bebafeddc97fa65
GDTran15 <trangiaiduong1512@gmail.com>
2025-12-05 09:40:03 +0700

    backend setup

---
+++ backend/src/main/java/com/share_money/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.share_money.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
